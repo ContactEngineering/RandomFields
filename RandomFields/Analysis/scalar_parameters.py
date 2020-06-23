3d26a5969d08bbede91cfecfbec73e1d09b507c3
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-06-23 14:58:03 +0200

    ENH: rms amplitudes

---
+++ RandomFields/Analysis/scalar_parameters.py
@@ -0,0 +1,23 @@
+
+
+
+def rms(amplitudes, reduction=np):
+    """
+    Compute the root mean square amplitude of a field stored on a uniform grid.
+
+    Parameters
+    ----------
+    amplitudes : :obj:`np.array`
+        Array containing the amplitudes of the field
+    reduction: default: numpy module
+        class or module implementing reduction operation sum
+    Returns
+    -------
+    rms_height : float
+        Root mean square height value.
+    """
+    n = np.prod(amplitudes.shape)
+    # if topography.is_MPI:
+
+    return np.sqrt(
+        reduction.sum((amplitudes - pnp.sum(amplitudes) / n) ** 2) / n)
