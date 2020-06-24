fb35d0e2ada1d52ec0b827945fad8a8573614b50
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-06-24 15:22:12 +0200

    TST: remove tests using slope

--- test/_test_generation.py
+++ test/_test_generation.py
@@ -4,25 +4,6 @@ import pytest
 import numpy as np
 from RandomFields.Analysis.scalar_parameters import rms
 
-# @pytest.mark.parametrize("n", [128, 129])
-# def test_fourier_synthesis(n):
-#     H = 0.74
-#     rms_slope = 1.2
-#     qs = 2 * np.pi / (0.4e-9)  # 1/m
-#     s = 2e-6
-
-#     topography = fourier_synthesis((n, n), (s, s),
-#                                    H,
-#                                    rms_slope=rms_slope,
-#                                    long_cutoff=s / 4,
-#                                    short_cutoff=4 * s / n)
-#     topography = Topography(topography, (s, s), periodic=True)
-#     qx, psdx = topography.power_spectrum_1D()
-#     qy, psdy = topography.transpose().power_spectrum_1D()
-
-#     assert psdy[-1] < 10 * psdx[-1]  # assert psdy is not much bigger
-#     assert abs(topography.rms_slope() - rms_slope) / rms_slope < 1e-1
-
 
 def test_fourier_synthesis_rms_height_more_wavevectors():
     """
@@ -50,7 +31,7 @@ def test_fourier_synthesis_rms_height_more_wavevectors():
         realised_rms_heights.append(rms(topography))
     # print(abs(np.mean(realised_rms_heights) - rms_height) / rms_height)
     assert abs(np.mean(realised_rms_heights) - rms_height) / \
-        rms_height < 0.1  # TODO: this is not very accurate !
+           rms_height < 0.1  # TODO: this is not very accurate !
 
 
 def test_fourier_synthesis_rms_height():
@@ -70,52 +51,7 @@ def test_fourier_synthesis_rms_height():
                                        )
         realised_rms_heights.append(rms(topography))
     assert abs(np.mean(realised_rms_heights) - rms_height) / \
-        rms_height < 0.3  # TODO: this is not very accurate !
-
-@pytest.mark.skip("slopes not yet implemented")
-def test_fourier_synthesis_c0():
-    H = 0.7
-    c0 = 8.
-    n = 512
-    s = n * 4.
-    ls = 8
-    qs = 2 * np.pi / ls
-    np.random.seed(0)
-    topography = fourier_synthesis((n, n), (s, s),
-                                   H,
-                                   c0=c0,
-                                   long_cutoff=s / 2,
-                                   short_cutoff=ls,
-                                   amplitude_distribution=lambda n: np.ones(n)
-                                   )
-    topography = Topography(topography, (s, s), periodic=True)
-    ref_slope = np.sqrt(1 / (4 * np.pi) * c0 / (1 - H) * qs ** (2 - 2 * H))
-    assert abs(topography.rms_slope() - ref_slope) / ref_slope < 1e-1
-
-    if False:
-        import matplotlib.pyplot as plt
-        q, psd = topography.power_spectrum_2D()
-
-        fig, ax = plt.subplots()
-        ax.loglog(q, psd, label="generated data")
-        ax.loglog(q, c0 * q ** (-2 - 2 * H), "--", label=r"$c_0 q^{-2-2H}$")
-
-        ax.set_xlabel("q")
-        ax.set_ylabel(r"$C^{iso}$")
-        ax.legend()
-        ax.set_ylim(bottom=1)
-        plt.show(block=True)
-
-        q, psd = topography.power_spectrum_1D()
-        fig, ax = plt.subplots()
-        ax.loglog(q, psd, label="generated data")
-        ax.loglog(q, c0 / np.pi * q ** (-1 - 2 * H),
-                  "--", label=r"$c_0 q^{-1-2H}$")
-
-        ax.legend()
-        ax.set_xlabel("q")
-        ax.set_ylabel(r"$C^{1D}$")
-        plt.show(block=True)
+           rms_height < 0.3  # TODO: this is not very accurate !
 
 
 def test_fourier_synthesis_1D_input():
@@ -134,66 +70,6 @@ def test_fourier_synthesis_1D_input():
                                    short_cutoff=ls,
                                    amplitude_distribution=lambda n: np.ones(n)
                                    )
-
-
-@pytest.mark.skip("slope not implemented yet")
-@pytest.mark.parametrize("n", (256, 1024))
-def test_fourier_synthesis_linescan_c0(n):
-    H = 0.7
-    c0 = 8.
-
-    s = n * 4.
-    ls = 32
-    qs = 2 * np.pi / ls
-    np.random.seed(0)
-    t = fourier_synthesis(
-        (n,), (s,),
-        c0=c0,
-        hurst=H,
-        long_cutoff=s / 2,
-        short_cutoff=ls,
-        amplitude_distribution=lambda n: np.ones(n)
-    )
-    t = Topography(t[:, 0], (s), periodic=True)
-    if False:
-        import matplotlib.pyplot as plt
-        q, psd = t.power_spectrum_1D()
-
-        fig, ax = plt.subplots()
-        ax.plot(q, psd)
-        ax.plot(q, c0 * q ** (-1 - 2 * H))
-
-        ax.set_xscale("log")
-        ax.set_yscale("log")
-
-        ax.set_ylim(bottom=1)
-        fig.show()
-
-    ref_slope = np.sqrt(1 / (2 * np.pi) * c0 / (1 - H) * qs ** (2 - 2 * H))
-    assert abs(t.rms_slope() - ref_slope) / ref_slope < 1e-1
-
-@pytest.mark.skip("slope not implemented yet")
-def test_fourier_synthesis_linescan_hprms():
-    H = 0.7
-    hprms = .2
-
-    n = 2048
-    s = n * 4.
-    ls = 64
-    qs = 2 * np.pi / ls
-    # np.random.seed(0)
-    realised_rms_slopes = []
-    for i in range(20):
-        t = fourier_synthesis((n,), (s,),
-                              rms_slope=hprms,
-                              hurst=H,
-                              long_cutoff=s / 2,
-                              short_cutoff=ls,
-                              )
-        t = Topography(t[:, 0], (s, ), periodic=True)
-        realised_rms_slopes.append(t.rms_slope())
-    ref_slope = hprms
-    assert abs(np.mean(realised_rms_slopes) - ref_slope) / ref_slope < 1e-1
 
 
 def test_fourier_synthesis_linescan_hrms_more_wavevectors():
@@ -216,12 +92,12 @@ def test_fourier_synthesis_linescan_hrms_more_wavevectors():
                               rms_height=hrms,
                               hurst=H,
                               rolloff=0,
-                              long_cutoff=s/8,
+                              long_cutoff=s / 8,
                               short_cutoff=ls,
                               )
         realised_rms_heights.append(rms(t))
     realised_rms_heights = np.array(realised_rms_heights)
     ref_height = hrms
-    #print(np.sqrt(np.mean((realised_rms_heights - np.mean(realised_rms_heights))**2)))
+    # print(np.sqrt(np.mean((realised_rms_heights - np.mean(realised_rms_heights))**2)))
     assert abs(np.mean(realised_rms_heights) -
                ref_height) / ref_height < 0.1  #
